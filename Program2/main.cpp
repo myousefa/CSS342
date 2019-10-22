c76d090fe24a95cfbe122b274e0a8cb7a026572c
myousefa <yousefali.m44@gmail.com>
2019-10-21 22:23:54 -0700

    hot

--- Program2/main.cpp
+++ Program2/main.cpp
@@ -5,7 +5,6 @@
  * It checks if brackets, parenthesis, or curly braces
  * are opened and closed in the correct order. 
  * 
- * This is a change, hello hi hey!
  **/
 
 #include <iostream>
@@ -13,108 +12,6 @@
 #include <stack>
 #include <string>
 using namespace std;
-
-// stack<char> fillingStack(string expression, string x){
-//     for(int i = 0; i < expression.length(); i++){
-//         stack <char> myStack;
-//         //push opening braces into stack 
-//         if(expression[i] == '{' || expression[i] == '(' || expression[i] == '['){
-//             myStack.push((expression[i]));
-//         } else { return myStack; }
-//         return myStack;
-//     }
-// }
-
-// bool BalacingExpression (string expression, char elements[]){
-//     stack<char> myStack;
-//     int index = 0; 
-
-//     for(int i = 0; i < expression.length(); i++){
-//         //push opening braces into stack 
-//         if(expression[i] == '{' || expression[i] == '(' || expression[i] == '['){
-//             myStack.push((expression[i]));
-//             continue;
-//         } else if(myStack.empty()){ return true; }
-//     }
-
-//     while(elements[index]){
-//         if(elements[index]==')' || elements[index]=='}' || elements[index]==']') {
-//             if (myStack.top() != elements[i]){
-//                 return false;
-//             }
-//             else {
-//                 myStack.pop();
-//             }
-//         }
-//     index++;
-//     }
-//     return myStack.empty();
-// }
-
-// bool BalancedExpression(string expression){
-//     //stack to put string into
-//     stack <char> myStack;
-//     //using for loop to put string elements into stack
-    
-    
-//         //check for closing braces
-//         if(expression[i] == '}'){
-//             //check if top element in stack is equal to closing brace and pop
-//             if(!myStack.empty() && myStack.top() == '{'){
-//                 myStack.pop();
-//             } else { return false; }
-//         } else if(expression[i]== ')'){
-//             if(!myStack.empty() && myStack.top() == ')'){
-//                 myStack.pop();
-//             } else { return false; } 
-//         } else if(expression[i]== ']'){
-//             if(!myStack.empty() && myStack.top() == ']'){
-//                 myStack.pop();
-//             } else { return false; }   
-//         }
-//         //if the stack is empty by the end it means expression is balanced
-//         if(myStack.empty()){
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-// bool isValid(string expression){
-//     stack<char> myStack;
-//     //if string is empty automatically return it is balanced
-//     if(expression.length() == 0){
-//         return true;
-//     }
-//     //any odd number expression can not be balanced
-//     if(expression.length() % 2 != 0){
-//         return false;
-//     }
-
-//     for(int i = 0; i < expression.length(); i++) {
-//         char c = expression[i];
-//         //cout << c << endl;
-//         if(c == '(' || c == '[' || c == '{'){
-//             myStack.push(c);
-//         }
-//         else if(c == '}' && myStack.top() == '{') {
-//             myStack.pop();
-//             return true;
-//         }
-//         else if(c == ')' && myStack.top() == '(') {
-//             myStack.pop();
-//             return true;
-//         }
-//         else if(c == ']' && myStack.top() == '[') {
-//             myStack.pop();
-//             return true;
-//         }
-//         return myStack.empty();
-//     }
-//     return myStack.empty();
-// }
 
 bool MatchingPair(char open, char close){
     if(open == '(' && close == ')'){
@@ -163,8 +60,6 @@ bool isValid(string expression){
     }
     return myStack.empty();
 }
-        
-
 
 int main(){
     //example expression
